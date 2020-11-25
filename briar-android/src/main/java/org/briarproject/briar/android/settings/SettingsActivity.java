5a4c5e6e0a6b9e309a5edaa40667842bf3edc2cd
Sebastian Kürten <sebastian@mobanisto.de>
2020-11-25 10:09:47 +0100

    When clicking profile image, open intent to select image

--- briar-android/src/main/java/org/briarproject/briar/android/settings/SettingsActivity.java
+++ briar-android/src/main/java/org/briarproject/briar/android/settings/SettingsActivity.java
@@ -1,17 +1,15 @@
 package org.briarproject.briar.android.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 
 import org.briarproject.bramble.api.db.DbException;
 import org.briarproject.bramble.api.identity.AuthorId;
-import org.briarproject.bramble.api.identity.IdentityManager;
-import org.briarproject.bramble.api.settings.SettingsManager;
 import org.briarproject.briar.R;
 import org.briarproject.briar.android.activity.ActivityComponent;
 import org.briarproject.briar.android.activity.BriarActivity;
-import org.briarproject.briar.android.navdrawer.NavDrawerViewModel;
 
 import javax.inject.Inject;
 
@@ -21,7 +19,14 @@ import androidx.lifecycle.ViewModelProviders;
 import de.hdodenhof.circleimageview.CircleImageView;
 import im.delight.android.identicons.IdenticonDrawable;
 
-import static java.util.Objects.requireNonNull;
+import static android.content.Intent.ACTION_GET_CONTENT;
+import static android.content.Intent.ACTION_OPEN_DOCUMENT;
+import static android.content.Intent.CATEGORY_OPENABLE;
+import static android.content.Intent.EXTRA_ALLOW_MULTIPLE;
+import static android.content.Intent.EXTRA_MIME_TYPES;
+import static android.os.Build.VERSION.SDK_INT;
+import static org.briarproject.bramble.util.AndroidUtils.getSupportedImageContentTypes;
+import static org.briarproject.briar.android.activity.RequestCodes.REQUEST_AVATAR_IMAGE;
 
 public class SettingsActivity extends BriarActivity {
 
@@ -49,12 +54,17 @@ public class SettingsActivity extends BriarActivity {
 		AuthorId authorId = null;
 		try {
 			authorId = settingsViewModel.getOwnAuthorId();
-		CircleImageView avatar = findViewById(R.id.avatarView);
-		avatar.setImageDrawable(
-				new IdenticonDrawable(authorId.getBytes()));
+			CircleImageView avatar = findViewById(R.id.avatarImage);
+			avatar.setImageDrawable(
+					new IdenticonDrawable(authorId.getBytes()));
 		} catch (DbException e) {
 			e.printStackTrace();
 		}
+
+		View avatarGroup = findViewById(R.id.avatarGroup);
+		avatarGroup.setOnClickListener(e -> {
+			selectAvatarImage();
+		});
 	}
 
 	@Override
@@ -70,4 +80,23 @@ public class SettingsActivity extends BriarActivity {
 		}
 		return false;
 	}
+
+	private void selectAvatarImage() {
+		Intent intent = getAvatarFileIntent();
+		startActivityForResult(intent, REQUEST_AVATAR_IMAGE);
+	}
+
+	private Intent getAvatarFileIntent() {
+		// TODO: I copied this from TextAttachmentController#getAttachFileIntent,
+		// maybe we should centralize it into a utility class.
+		Intent intent = new Intent(SDK_INT >= 19 ?
+				ACTION_OPEN_DOCUMENT : ACTION_GET_CONTENT);
+		intent.setType("image/*");
+		intent.addCategory(CATEGORY_OPENABLE);
+		if (SDK_INT >= 19)
+			intent.putExtra(EXTRA_MIME_TYPES, getSupportedImageContentTypes());
+		if (SDK_INT >= 18) intent.putExtra(EXTRA_ALLOW_MULTIPLE, true);
+		return intent;
+	}
+
 }
