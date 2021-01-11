9eaf1d21bbf35ccfd3a2717b8389dbcb9805eb15
Torsten Grote <t@grobox.de>
2021-01-11 16:36:15 -0300

    Turn FeedController into FeedViewModel

--- briar-android/src/main/java/org/briarproject/briar/android/AppModule.java
+++ briar-android/src/main/java/org/briarproject/briar/android/AppModule.java
@@ -30,15 +30,16 @@ import org.briarproject.bramble.util.StringUtils;
 import org.briarproject.briar.android.account.DozeHelperModule;
 import org.briarproject.briar.android.account.LockManagerImpl;
 import org.briarproject.briar.android.account.SetupModule;
+import org.briarproject.briar.android.blog.BlogModule;
 import org.briarproject.briar.android.contact.ContactListModule;
 import org.briarproject.briar.android.forum.ForumModule;
 import org.briarproject.briar.android.keyagreement.ContactExchangeModule;
 import org.briarproject.briar.android.login.LoginModule;
 import org.briarproject.briar.android.navdrawer.NavDrawerModule;
 import org.briarproject.briar.android.privategroup.conversation.GroupConversationModule;
-import org.briarproject.briar.android.settings.SettingsModule;
 import org.briarproject.briar.android.privategroup.list.GroupListModule;
 import org.briarproject.briar.android.reporting.DevReportModule;
+import org.briarproject.briar.android.settings.SettingsModule;
 import org.briarproject.briar.android.sharing.SharingModule;
 import org.briarproject.briar.android.test.TestAvatarCreatorImpl;
 import org.briarproject.briar.android.viewmodel.ViewModelModule;
@@ -81,6 +82,7 @@ import static org.briarproject.briar.android.TestingConstants.IS_DEBUG_BUILD;
 		DevReportModule.class,
 		ContactListModule.class,
 		// below need to be within same scope as ViewModelProvider.Factory
+		BlogModule.BindsModule.class,
 		ForumModule.class,
 		GroupListModule.class,
 		GroupConversationModule.class,
